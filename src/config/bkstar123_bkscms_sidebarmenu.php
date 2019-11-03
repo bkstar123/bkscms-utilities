4d001ccb7176e7305c0e786221dd10007cd3d4d7
Hoang Anh Tuan <hoangtuandhbk@gmail.com>
2019-11-03 14:38:56 +0700

    In progress

--- src/config/bkstar123_bkscms_sidebarmenu.php
+++ src/config/bkstar123_bkscms_sidebarmenu.php
@@ -6,10 +6,57 @@
  * 'path' for an expandable link should be '#'
  */
 return [
-    // [
-    //     'name' => 'Link Name',
-    // 	'path' => '/link/path',
-    // 	'icon' => 'link icon',
-    // 	'children' => []
-    // ],
+    [
+        'name' => 'Admin Managment',
+        'path' => '#',
+        'icon' => 'far fa-user',
+        'children' => [
+            [
+                'name' => 'Admins',
+                'path' => '/cms/admins',
+                'icon' => 'fa fa-users',
+            ],
+            [
+                'name' => 'Create Admin',
+                'path' => '/cms/admins/create',
+                'icon' => 'fa fa-user-plus',
+            ]
+        ]
+    ],
+
+    [
+        'name' => 'Role Managment',
+        'path' => '#',
+        'icon' => 'fa fa-certificate',
+        'children' => [
+            [
+                'name' => 'Roles',
+                'path' => '/cms/roles',
+                'icon' => 'fa fa-user-circle',
+            ],
+            [
+                'name' => 'Create Role',
+                'path' => '/cms/roles/create',
+                'icon' => 'fa fa-plus',
+            ]
+        ]
+    ],
+
+    [
+        'name' => 'Permission Managment',
+        'path' => '#',
+        'icon' => 'fa fa-universal-access',
+        'children' => [
+            [
+                'name' => 'Permissions',
+                'path' => '/cms/permissions',
+                'icon' => 'fa fa-ship',
+            ],
+            [
+                'name' => 'Create Permission',
+                'path' => '/cms/permissions/create',
+                'icon' => 'fa fa-plus',
+            ]
+        ]
+    ],
 ];
